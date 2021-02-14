6f9e86beb23a085a13f61327a84c9e5484863b2e
Nick <bigbasspoll@gmail.com>
2021-02-14 23:19:01 +0300

    finalProject

--- js/script.js
+++ js/script.js
@@ -5,7 +5,6 @@ $(document).ready(function($){
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
         cssEase: 'linear',
-        mobileFirst: true,
         autoplay: true,
         autoplaySpeed: 2000,
         speed: 1200,
@@ -19,7 +18,7 @@ $(document).ready(function($){
                 dots: true
               }
             },
-            
+ 
         ]
     });
 
